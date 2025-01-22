3f644048d7ef8d8cb95318fde0c80cee0c7f5bb2
jaydenhs <j3hsiao@uwaterloo.ca>
2025-01-22 14:48:29 -0500

    Reorganize agent file

--- 3-drawing/faces/agent.js
+++ 3-drawing/faces/agent.js
@@ -14,6 +14,20 @@ class Agent {
     this.reset();
   }
 
+  reset() {
+    this.dur = p.duration;
+    this.radius = random(10, 150);
+    this.startAngle = random(TWO_PI);
+    this.framesActive = 0;
+
+    this.startColor = this.colors[this.active];
+    this.endColor = this.colors[(this.active + 1) % this.colors.length];
+    this.startColor[3] = p.alpha;
+    this.endColor[3] = p.alpha;
+
+    this.active = (this.active + 1) % this.colors.length;
+  }
+
   update() {
     this.framesActive++;
 
@@ -36,12 +50,6 @@ class Agent {
     }
   }
 
-  updateColors() {
-    this.tempColor = this.startColor;
-    this.startColor = this.endColor;
-    this.endColor = this.tempColor;
-  }
-
   draw() {
     // draw a line between last position
     // and current position
@@ -51,18 +59,10 @@ class Agent {
     line(this.lastX, this.lastY, this.x, this.y);
   }
 
-  reset() {
-    this.dur = p.duration;
-    this.radius = random(10, 150);
-    this.startAngle = random(TWO_PI);
-    this.framesActive = 0;
-
-    this.startColor = this.colors[this.active];
-    this.endColor = this.colors[(this.active + 1) % this.colors.length];
-    this.startColor[3] = p.alpha;
-    this.endColor[3] = p.alpha;
-
-    this.active = (this.active + 1) % this.colors.length;
+  updateColors() {
+    this.tempColor = this.startColor;
+    this.startColor = this.endColor;
+    this.endColor = this.tempColor;
   }
 }
 
